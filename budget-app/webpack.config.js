045b0e6719e0b1a543452cf353d3d7b0e1edbd9f
edddie212 <45018910+edddie212@users.noreply.github.com>
2021-12-19 00:11:47 +0200

    Add files via upload

---
+++ budget-app/webpack.config.js
@@ -0,0 +1,28 @@
+//entry -> output
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'scripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules:[{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        },
+            {
+              test: /\.s?css$/,
+              use:['style-loader', 'css-loader','sass-loader']
+            }],
+    },
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+        compress: true,
+        port: 8080,
+        historyApiFallback: true
+    }
+};
